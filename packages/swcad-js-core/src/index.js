ed5e33a780abbcaf91782f04d2c4b345edb92c81
RJ Salvador <salvador.workshop@gmail.com>
2026-03-31 09:13:38 -0400

    Updoot

--- packages/swcad-js-core/src/index.js
+++ packages/swcad-js-core/src/index.js
@@ -2,8 +2,13 @@ const coreModule = require('./core');
 const utilsModule = require('./utils');
 const modelModule = require('./models');
 
+const stdSpecsModule = require('swcad-js-std-specs');
+const familiesModule = require('swcad-js-families');
+const uiModule = require('swcad-js-ui');
+const buildersModule = require('swcad-js-builders');
+
 const init = ({ lib }) => {
-    const stdSpecs = require('sw-jscad-std-specs').init({ lib });
+    const stdSpecs = stdSpecsModule.init({ lib });
     stdSpecs.constants.SILVER_RATIO = 1 + Math.sqrt(2)          // 2.4142
     stdSpecs.constants.BRONZE_RATIO = 3 + Math.sqrt(13) / 2     // 3.3028
     stdSpecs.constants.COPPER_RATIO = 2 + Math.sqrt(5)          // 4.2361
@@ -17,7 +22,18 @@ const init = ({ lib }) => {
     swJscad.utils = utilsModule.init({ lib, swLib: swJscad });
     swJscad.models = modelModule.init({ lib, swLib: swJscad });
 
-    return swJscad;
+    const swcadJsFamilies = familiesModule.init({ lib, swLib: swcadJsCore })
+    const swcadJsUi = uiModule.init({ lib, swLib: swcadJsCore })
+    const swcadJsBuilders = buildersModule.init({ lib, swLib: swcadJsCore, swFamilies: swcadJsFamilies })
+
+    const swcadJs = {
+        ...swcadJsCore,
+        families: swcadJsFamilies,
+        ui: swcadJsUi,
+        builders: swcadJsBuilders,
+    }
+
+    return swcadJs;
 }
 
 module.exports = { init };
