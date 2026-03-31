8089c1c816be595b3c1f0ab007465d8764d1ddb0
RJ Salvador <salvador.workshop@gmail.com>
2026-03-31 05:33:51 -0400

    More cleanup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
- 
+const stdSpecsModule = require('swcad-js-std-specs');
+const coreModule = require('swcad-js-core');
+const familiesModule = require('swcad-js-families');
+const uiModule = require('swcad-js-ui');
+const buildersModule = require('swcad-js-builders');
+
+const init = ({ lib }) => {
+    const swcadJsCore = coreModule.init({ lib })
+    
+
+    return swcadJs;
+}
+
+module.exports = { init };
