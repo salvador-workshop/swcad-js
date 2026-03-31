cc1f05f59f9f00c669042f425d0f08776901cba1
RJ Salvador <salvador.workshop@gmail.com>
2026-03-31 09:21:57 -0400

    more updoot

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-const stdSpecsModule = require('swcad-js-std-specs');
 const coreModule = require('swcad-js-core');
-const familiesModule = require('swcad-js-families');
-const uiModule = require('swcad-js-ui');
-const buildersModule = require('swcad-js-builders');
 
 const init = ({ lib }) => {
-    const swcadJsCore = coreModule.init({ lib })
-
-    const swcadJsFamilies = coreModule.init({ lib, swLib: swcadJsCore })
-    const swcadJsUi = coreModule.init({ lib, swLib: swcadJsCore })
-
-    const swcadJsBuilders = coreModule.init({ lib, swLib: swcadJsCore, swFamilies: swcadJsFamilies })
-
-    const swcadJs = {
-        ...swcadJsCore,
-        ui: swcadJsUi,
-        builders: swcadJsBuilders,
-    }
-
+    const swcadJs = coreModule.init({ lib })
     return swcadJs;
 }
 
