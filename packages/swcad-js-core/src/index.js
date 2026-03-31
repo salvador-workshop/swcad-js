2496e4581b60cd4db96b2efddc726dfb27496e35
RJ Salvador <salvador.workshop@gmail.com>
2026-03-31 10:59:52 -0400

    Reverting and reworking

--- packages/swcad-js-core/src/index.js
+++ packages/swcad-js-core/src/index.js
@@ -1,14 +1,16 @@
+// TODO: Replace relative paths with package imports once build tools are in place
+const stdSpecsPackage = require('../../swcad-js-std-specs');
+const familiesPackage = require('../../swcad-js-families');
+const uiPackage = require('../../swcad-js-ui');
+const buildersPackage = require('../../swcad-js-builders');
+
 const coreModule = require('./core');
 const utilsModule = require('./utils');
 const modelModule = require('./models');
 
-const stdSpecsModule = require('swcad-js-std-specs');
-const familiesModule = require('swcad-js-families');
-const uiModule = require('swcad-js-ui');
-const buildersModule = require('swcad-js-builders');
-
 const init = ({ lib }) => {
-    const stdSpecs = stdSpecsModule.init({ lib });
+    const stdSpecs = stdSpecsPackage.init({ lib });
+
     stdSpecs.constants.SILVER_RATIO = 1 + Math.sqrt(2)          // 2.4142
     stdSpecs.constants.BRONZE_RATIO = 3 + Math.sqrt(13) / 2     // 3.3028
     stdSpecs.constants.COPPER_RATIO = 2 + Math.sqrt(5)          // 4.2361
@@ -22,9 +24,9 @@ const init = ({ lib }) => {
     swcadJsCore.utils = utilsModule.init({ lib, swLib: swcadJsCore });
     swcadJsCore.models = modelModule.init({ lib, swLib: swcadJsCore });
 
-    const swcadJsFamilies = familiesModule.init({ lib, swLib: swcadJsCore })
-    const swcadJsUi = uiModule.init({ lib, swLib: swcadJsCore })
-    const swcadJsBuilders = buildersModule.init({ lib, swLib: swcadJsCore, swFamilies: swcadJsFamilies })
+    const swcadJsFamilies = familiesPackage.init({ lib, swLib: swcadJsCore })
+    const swcadJsUi = uiPackage.init({ lib, swLib: swcadJsCore })
+    const swcadJsBuilders = buildersPackage.init({ lib, swLib: swcadJsCore, swFamilies: swcadJsFamilies })
 
     const swcadJs = {
         ...swcadJsCore,
