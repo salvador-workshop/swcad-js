4a21514b71bea6feb690d309feb2a03b05fe80cb
RJ Salvador <salvador.workshop@gmail.com>
2026-04-05 18:51:30 -0400

    Added more build configs

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,5 +1,7 @@
 import * as esbuild from 'esbuild'
 
+// Build the main library
+
 await esbuild.buildSync({
   entryPoints: ['src/index.js'],
   bundle: true,
@@ -13,6 +15,26 @@ await esbuild.buildSync({
   bundle: true,
   minify: true,
   outfile: 'dist/swcad.min.js',
+  target: 'es2020',
+  format: 'cjs',
+})
+
+
+// Build the debug versions
+
+await esbuild.buildSync({
+  entryPoints: ['src/debug.js'],
+  bundle: true,
+  outfile: 'dist/swcad.debug.js',
+  target: 'es2020',
+  format: 'cjs',
+})
+
+await esbuild.buildSync({
+  entryPoints: ['src/debug.js'],
+  bundle: true,
+  minify: true,
+  outfile: 'dist/swcad.debug.min.js',
   target: 'es2020',
   format: 'cjs',
 })
---
+++ src/debug.js
@@ -0,0 +1,2 @@
+const coreModule = require('../packages/swcad-js-core');
+const swcadJs = coreModule.init({ jscad });
