8dec051e9c669bd4f6b246089a12dea4741a63ae
RJ Salvador <salvador.workshop@gmail.com>
2026-04-10 19:18:34 -0400

    smolfix

--- packages/swcad-js-profiles/src/structure/index.js
+++ packages/swcad-js-profiles/src/structure/index.js
@@ -3,7 +3,7 @@
 const structureInit = ({ jscad, swcadJs }) => {
     const structure = {
         foils: require('./foils-2d').init({ jscad, swcadJs }),
-        mesh: require('../mesh-2d').init({ jscad, swcadJs }),
+        mesh: require('./mesh-2d').init({ jscad, swcadJs }),
         arch: require('./arch-2d').init({ jscad, swcadJs }),
     }
 
